790489af35444d81e530b7b5932c12146f745deb
Alexandre Benoit <alexandre.benoit@r01p08.lv322.lyn-vai.sm.cri.epita.fr>
2026-09-03 20:05:26 +0200

    ddddd

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults nvim ulauncher polybar picom"
-packages_list='hyfetch nerd-fonts oh-my-zsh neovim polybar ulauncher picom'
+dot_list="bashrc zshrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults nvim ulauncher polybar picom alacritty kitty"
+packages_list='hyfetch nerd-fonts.fira-code neovim polybar ulauncher picom zsh zsh-autosuggestions zsh-syntax-highlighting kitty nerd-fonts.jetbrains-mono eza'
 
 for package in $packages_list;
 do
@@ -13,4 +13,9 @@ for f in $dot_list; do
   ln -s "$AFS_DIR/.confs/$f" "$HOME/.$f"
 done
 
+if which zsh; then
+  export SHELL="$(which zsh)"
+fi
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 polybar
