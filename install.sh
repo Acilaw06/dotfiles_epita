17a94cc476106c115a318b6beaf756c1093749a5
Alexandre BENOIT <alexandre.benoit.06@gmail.com>
2026-09-03 22:01:04 +0200

    change_installsh

--- install.sh
+++ install.sh
@@ -3,10 +3,9 @@
 dot_list="bashrc zshrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh thunderbird vim vimrc Xdefaults nvim ulauncher polybar picom alacritty kitty"
 packages_list='hyfetch nerd-fonts.fira-code neovim polybar ulauncher picom zsh zsh-autosuggestions zsh-syntax-highlighting kitty nerd-fonts.jetbrains-mono eza'
 
-for package in $packages_list;
-do
+for package in $packages_list; do
   nix profile install nixpkgs#$package
-done;
+done
 
 for f in $dot_list; do
   rm -rf "$HOME/.$f"
@@ -17,5 +16,5 @@ if which zsh; then
   export SHELL="$(which zsh)"
 fi
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 polybar
